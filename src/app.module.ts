f353ee8c1aa5a39d24fbdface663652cb6bc520b
Imtiaz Ahmed <imtiazali78652@gmail.com>
2025-06-23 21:51:23 +0500

    last

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ItemsModule } from './items/items.module';
 import { DoctorController } from './doctor/doctor.controller';
 import { DoctorService } from './doctor/doctor.service';
 import keys from './config/keys';
 import { DoctorModule } from './doctor/doctor.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://imtiazali78652:MdfObcZvaUvKogq1@cluster0.ztdnf5k.mongodb.net/',
     ),
-    ItemsModule,
-    DoctorModule
+    DoctorModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
