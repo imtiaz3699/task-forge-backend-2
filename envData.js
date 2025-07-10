c977ce8d05ec0535333b0d1677ad377f658f986e
Imtiaz Ahmed <imtiazali78652@gmail.com>
2025-07-10 23:09:48 +0500

    env data updated

--- envData.js
+++ envData.js
@@ -1,1 +1,7 @@
 //# sourceMappingURL=envData.js.map
+
+// MONGO_URI = mongodb+srv://imtiazali78652:MdfObcZvaUvKogq1@cluster0.ztdnf5k.mongodb.net/
+// JWT_SECRET = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30
+// CLOUDINARY_SECRET = UkP2AHZyhMhdZ8FTfisva-m05OU
+// CLOUDINARY_CLOUD_NAME = ddx3egvut
+// CLOUDINARY_API = 683157957723984
